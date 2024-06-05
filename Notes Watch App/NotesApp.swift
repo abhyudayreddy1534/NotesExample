439b9d4137c48043fdc3656865419819717ee29e
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-06-05 15:48:16 -0500

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Sravanthi Chinthireddy on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
